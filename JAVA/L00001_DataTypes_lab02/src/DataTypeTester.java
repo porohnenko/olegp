e79a644d31587372f26070970aa6e5f4324b9305
porohnenko <porohnenko@mail.ru>
2016-02-24 13:24:09 +0300

    This folder for JAVA labs and projects

---
+++ JAVA/L00001_DataTypes_lab02/src/DataTypeTester.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author olegp
+ */
+public class DataTypeTester {
+    public static void intTesting(){
+    int x = 20;
+    int y = 7;
+    int z;
+    
+    //Arithmetic operators
+    System.out.print("\t\"int\" with Arithmetic operators");
+    
+    z = x + y;
+    System.out.printf("\n%d + %d = %d",x,y,z);
+    
+    z = x - y;
+    System.out.printf("\n%d - %d = %d",x,y,z);
+    
+    z = x * y;
+    System.out.printf("\n%d * %d = %d",x,y,z);
+    
+    z = x / y;
+    System.out.printf("\n%d / %d = %d",x,y,z);
+    
+    z = x % y;
+    System.out.printf("\n%d %% %d = %d",x,y,z);
+    
+    System.out.printf("\n++%d = %d",z,++z);
+    System.out.printf("\n--%d = %d",z,--z);
+    System.out.printf("\n%d++ = %d",z,z++);
+    System.out.printf("\n%d-- = %d",z,z--);
+    
+    z = +x;
+    System.out.printf("\n+%d = %d",x,z);
+    
+    z = -x;
+    System.out.printf("\n-%d = %d",x,z);
+    
+    System.out.print("\n\n\t\"int\" with Reletional Operators:");
+    
+    System.out.printf("\n%d > %d --> %b",x,y, x>y);
+    System.out.printf("\n%d >= %d --> %b",x,y, x>=y);
+    System.out.printf("\n%d < %d --> %b",x,y, x<y);
+    System.out.printf("\n%d <= %d --> %b",x,y, x<=y);
+    System.out.printf("\n%d == %d --> %b",x,y, x==y);
+    System.out.printf("\n%d != %d --> %b",x,y, x!=y);
+    }
+}
