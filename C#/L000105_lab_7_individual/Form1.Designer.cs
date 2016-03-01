e3d44be7d8e547c0566f3f8aae6b3ee9d37910f0
porohnenko <porohnenko@mail.ru>
2016-03-02 00:00:54 +0300

    lab7 and lab7_individual

---
+++ C#/L000105_lab_7_individual/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace L000105_lab_7_individual
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.richTextBox2.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox2.Location = new System.Drawing.Point(12, 60);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(680, 258);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(618, 330);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(524, 330);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(93, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(599, 23);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(13, 330);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(87, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Filter tags";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 365);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "HTML filter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ C#/L000105_lab_7_individual/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace L000105_lab_7_individual
+{
+    public partial class Form1 : Form
+    {
+        public string str;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Окрываем диалоговое окно
+            OpenFileDialog ofn = new OpenFileDialog();
+            ofn.InitialDirectory = "D:\\";              //Диск D:
+            ofn.Filter = "html files |*.html";          //Указываем тип открываемых файлов
+            ofn.FilterIndex = 2;
+
+            if (ofn.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = ofn.FileName;
+                richTextBox2.Text = File.ReadAllText(ofn.FileName);
+                str = richTextBox2.Text;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+       
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Алгоритм фильтрации
+            string pattern = @"<[^>]*>";
+            Regex newReg = new Regex(pattern);
+            richTextBox2.Text = Regex.Replace(str, @"<[^>]*>", String.Empty);
+            //удаление лишних строк и пробелов
+            //richTextBox2.Text = Regex.Replace(richTextBox2.Text, @"[\s\r\n]+", " ").Trim();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Диалоговое окно сохранения отформатированного файла
+            SaveFileDialog ofn1 = new SaveFileDialog();
+            ofn1.Filter = "txt files |*.txt";
+            ofn1.FilterIndex = 2;
+            MemoryStream memorystream = new System.IO.MemoryStream();
+            Stream filestream;
+            if (ofn1.ShowDialog() == DialogResult.OK)
+            {
+                //ассоциируем поток с именем файла - если фйла нет создаем
+                filestream = ofn1.OpenFile();
+                memorystream.Position = 0;
+                //сохраняем в поток содержимое richTextBox2
+                richTextBox2.SaveFile(memorystream,RichTextBoxStreamType.PlainText);
+                //переносим в файл информацию и закрываем поток
+                memorystream.WriteTo(filestream);
+                filestream.Close();
+            }
+        }
+    }
+}
