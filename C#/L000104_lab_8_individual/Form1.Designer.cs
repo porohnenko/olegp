6b62c9e5d0d8d746e2689e7b4fae7ef9e85fe65f
porohnenko <porohnenko@mail.ru>
2016-03-04 13:45:42 +0300

    lab_8_individual complete!

--- C#/L000104_lab_8_individual/Form1.Designer.cs
+++ C#/L000104_lab_8_individual/Form1.Designer.cs
@@ -20,6 +20,7 @@
             base.Dispose(disposing);
         }
 
+        
         #region Код, автоматически созданный конструктором форм Windows
 
         /// <summary>
@@ -28,6 +29,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -39,6 +41,10 @@
             this.button2 = new System.Windows.Forms.Button();
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -48,15 +54,17 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(92, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Load list";
+            this.button1.Text = "Load file";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // richTextBox1
             // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.richTextBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.richTextBox1.Location = new System.Drawing.Point(114, 17);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(277, 21);
+            this.richTextBox1.Size = new System.Drawing.Size(483, 21);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
             // 
@@ -72,50 +80,53 @@
             // 
             // comboBox2
             // 
+            this.comboBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.comboBox2.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(143, 83);
+            this.comboBox2.Location = new System.Drawing.Point(278, 83);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 23);
+            this.comboBox2.Size = new System.Drawing.Size(103, 23);
             this.comboBox2.TabIndex = 4;
             // 
             // comboBox1
             // 
+            this.comboBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.comboBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(16, 83);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.Size = new System.Drawing.Size(256, 23);
             this.comboBox1.TabIndex = 2;
             // 
             // comboBox3
             // 
+            this.comboBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.comboBox3.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(270, 83);
+            this.comboBox3.Location = new System.Drawing.Point(387, 83);
             this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(121, 23);
+            this.comboBox3.Size = new System.Drawing.Size(83, 23);
             this.comboBox3.TabIndex = 5;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(140, 67);
+            this.label2.Location = new System.Drawing.Point(275, 67);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(49, 15);
+            this.label2.Size = new System.Drawing.Size(42, 15);
             this.label2.TabIndex = 6;
-            this.label2.Text = "Period";
+            this.label2.Text = "Month";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(267, 67);
+            this.label3.Location = new System.Drawing.Point(384, 67);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(49, 15);
+            this.label3.Size = new System.Drawing.Size(35, 15);
             this.label3.TabIndex = 7;
-            this.label3.Text = "Salary";
+            this.label3.Text = "Year";
             // 
             // button2
             // 
@@ -126,20 +137,22 @@
             this.button2.TabIndex = 8;
             this.button2.Text = "Generate";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // richTextBox2
             // 
+            this.richTextBox2.BackColor = System.Drawing.Color.LightCyan;
             this.richTextBox2.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.richTextBox2.Location = new System.Drawing.Point(16, 156);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(375, 210);
+            this.richTextBox2.Size = new System.Drawing.Size(581, 210);
             this.richTextBox2.TabIndex = 9;
             this.richTextBox2.Text = "";
             // 
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button3.Location = new System.Drawing.Point(316, 385);
+            this.button3.Location = new System.Drawing.Point(522, 385);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 10;
@@ -147,11 +160,59 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(16, 385);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Save";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.Location = new System.Drawing.Point(97, 122);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Clear";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.comboBox4.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(476, 83);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 23);
+            this.comboBox4.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(473, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 15);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Salary";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(412, 425);
+            this.BackgroundImage = global::L000104_lab_8_individual.Properties.Resources.fon;
+            this.ClientSize = new System.Drawing.Size(613, 425);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.button2);
@@ -163,8 +224,11 @@
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SALARY";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -184,6 +248,10 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.RichTextBox richTextBox2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- C#/L000104_lab_8_individual/Form1.cs
+++ C#/L000104_lab_8_individual/Form1.cs
@@ -6,14 +6,89 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace L000104_lab_8_individual
 {
     public partial class Form1 : Form
     {
+
+
+        public class AttrMonth
+        {
+            public string Month;
+            public AttrMonth(string month)
+            {
+                this.Month = month;
+                //this.Salary = salary;
+            }
+        }
+
+        public class AttrYear
+        {
+            public int Year;
+            public AttrYear(int year)
+            {
+                this.Year = year;
+            }
+        }
+
+        public class AttrSalary
+        {
+            public int Salary;
+            public AttrSalary(int salary)
+            {
+                this.Salary = salary;
+            }
+        }
+
+        public class AttrPerson
+        {
+            public string Person;
+            public AttrPerson(string person)
+            {
+                this.Person = person;
+            }
+        }
+
+        AttrMonth[] att1 = new AttrMonth[15];
+        AttrYear[] att2 = new AttrYear[5];
+        AttrSalary[] att3 = new AttrSalary[10];
+        AttrPerson[] att4 = new AttrPerson[35];
+
         public Form1()
         {
             InitializeComponent();
+
+            //Month
+            att1[0] = new AttrMonth("January"); this.comboBox2.Items.Add(att1[0].Month);
+            att1[1] = new AttrMonth("February"); this.comboBox2.Items.Add(att1[1].Month);
+            att1[2] = new AttrMonth("March"); this.comboBox2.Items.Add(att1[2].Month);
+            att1[3] = new AttrMonth("April"); this.comboBox2.Items.Add(att1[3].Month);
+            att1[4] = new AttrMonth("May"); this.comboBox2.Items.Add(att1[4].Month);
+            att1[5] = new AttrMonth("June"); this.comboBox2.Items.Add(att1[5].Month);
+            att1[6] = new AttrMonth("July"); this.comboBox2.Items.Add(att1[6].Month);
+            att1[7] = new AttrMonth("August"); this.comboBox2.Items.Add(att1[7].Month);
+            att1[8] = new AttrMonth("September"); this.comboBox2.Items.Add(att1[8].Month);
+            att1[9] = new AttrMonth("October"); this.comboBox2.Items.Add(att1[9].Month);
+            att1[10] = new AttrMonth("November"); this.comboBox2.Items.Add(att1[10].Month);
+            att1[11] = new AttrMonth("December"); this.comboBox2.Items.Add(att1[11].Month);
+
+            //Year
+            att2[0] = new AttrYear(2015); this.comboBox3.Items.Add(att2[0].Year);
+            att2[1] = new AttrYear(2016); this.comboBox3.Items.Add(att2[1].Year);
+
+            //Salary
+            att3[0] = new AttrSalary(9000000); this.comboBox4.Items.Add(att3[0].Salary);
+            att3[1] = new AttrSalary(10000000); this.comboBox4.Items.Add(att3[1].Salary);
+            att3[2] = new AttrSalary(11000000); this.comboBox4.Items.Add(att3[2].Salary);
+            att3[3] = new AttrSalary(12000000); this.comboBox4.Items.Add(att3[3].Salary);
+            att3[4] = new AttrSalary(13000000); this.comboBox4.Items.Add(att3[4].Salary);
+            att3[5] = new AttrSalary(14000000); this.comboBox4.Items.Add(att3[5].Salary);
+
+
+
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -21,6 +96,96 @@ namespace L000104_lab_8_individual
             this.Close();
         }
 
-        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Окрываем диалоговое окно
+            OpenFileDialog ofn = new OpenFileDialog();
+            ofn.InitialDirectory = "D:\\";              //Диск D:
+            ofn.Filter = "txt files |*.txt";          //Указываем тип открываемых файлов
+            ofn.FilterIndex = 2;
+
+            if (ofn.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.Text = ofn.FileName;
+                //richTextBox2.Text = File.ReadAllText(ofn.FileName);
+                //str = richTextBox1.Text;
+            }
+
+            StreamReader streamreader = new StreamReader(ofn.FileName);
+            String item;
+            while ((item = streamreader.ReadLine()) != null)
+            {
+                comboBox1.Items.Add(item);
+            }
+            streamreader.Close();
+
+            comboBox1.Text = "";
+            comboBox2.Text = "";
+            comboBox3.Text = "";
+            comboBox4.Text = "";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Диалоговое окно сохранения отформатированного файла
+            SaveFileDialog ofn1 = new SaveFileDialog();
+            ofn1.Filter = "txt files |*.txt";
+            ofn1.FilterIndex = 2;
+            MemoryStream memorystream = new System.IO.MemoryStream();
+            Stream filestream;
+            if (ofn1.ShowDialog() == DialogResult.OK)
+            {
+                //ассоциируем поток с именем файла - если файла нет создаем
+                filestream = ofn1.OpenFile();
+                memorystream.Position = 0;
+                //сохраняем в поток содержимое richTextBox2
+                richTextBox2.SaveFile(memorystream, RichTextBoxStreamType.PlainText);
+                //переносим в файл информацию и закрываем поток
+                memorystream.WriteTo(filestream);
+                filestream.Close();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Attention! All strings will be done!");       
+            DialogResult result = MessageBox.Show("Clear all data?", "Warning!!!", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                string nullString = "";
+                richTextBox2.Text = nullString;
+                comboBox1.Text = nullString;
+                comboBox2.Text = nullString;
+                comboBox3.Text = nullString;
+                comboBox4.Text = nullString;
+            }
+        }
+
+        //Button Generate
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == ""
+                     || comboBox2.Text == ""
+                     || comboBox3.Text == ""
+                     || comboBox4.Text == "")
+            {
+                MessageBox.Show("Please, fill all fields!");
+            }
+            else
+            {
+                richTextBox2.Text += "Person:  " + comboBox1.SelectedItem.ToString()
+                                    + "\nMonth:   " + comboBox2.SelectedItem.ToString()
+                                    + "\nYear:    " + comboBox3.SelectedItem.ToString()
+                                    + "\nSalary:  " + comboBox4.SelectedItem.ToString()
+                                    + "\n------------------------------------------------------------------------------\n";
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
     }
 }
