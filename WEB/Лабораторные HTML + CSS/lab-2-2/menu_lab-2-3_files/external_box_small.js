9fa0175afdbc68e6eb8e822b72797a0a50d112b7
porohnenko <porohnenko@mail.ru>
2016-03-18 20:57:04 +0300

    start for the my own site!

---
+++ WEB/Лабораторные HTML + CSS/lab-2-2/menu_lab-2-3_files/external_box_small.js
@@ -0,0 +1,3 @@
+document.write('\n'
+ + '<iframe src="http://www.yr.no/place/Belarus/Minsk/Minsk/external_box_small.html" width="180" height="322" frameborder="0" style="margin: 10px 0 10px 0" scrolling="no"></iframe>\n'
+);
