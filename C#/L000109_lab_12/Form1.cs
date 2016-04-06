81831643032917b273c039b1a39f1c97cffc4a7d
porohnenko <porohnenko@mail.ru>
2016-04-06 14:46:12 +0300

    !

---
+++ C#/L000109_lab_12/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace L000109_lab_12
+{
+    public partial class Form1 : Form
+    {
+        public string str;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x, y; string st;
+            x = Convert.ToDouble(textBox1.Text);
+            y = Math.Sin(x);
+            st = String.Format("{0,4:0.##}", y);
+            textBox2.Text = st;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //lab12_1
+            /*ToolTip toolTip2 = new ToolTip();
+
+            toolTip2.AutoPopDelay = 5000;
+            toolTip2.InitialDelay = 100;
+            toolTip2.ReshowDelay = 50;
+            toolTip2.BackColor = Color.Azure;
+            toolTip2.ShowAlways = true;
+            toolTip2.SetToolTip(this.button1, " ");
+            toolTip2.ToolTipTitle = "note...";*/
+
+            //lab12_2
+            /////////////////////////////////////////////////////////////////////////////////////////////////
+            // Create the ToolTip and associate with the Form container.
+            ToolTip toolTip2 = new ToolTip();
+
+            // Set up the delays for the ToolTip.
+            toolTip2.AutoPopDelay = 5000;
+            toolTip2.InitialDelay = 100;
+            toolTip2.ReshowDelay = 50;
+            toolTip2.BackColor = Color.Azure;
+            // Force the ToolTip text to be displayed whether or not the form is active.
+            toolTip2.ShowAlways = true;
+
+            // Set up the ToolTip text for the Button and Checkbox.
+            toolTip2.SetToolTip(this.button1, " ");
+            toolTip2.ToolTipTitle = "4444444";
+            /////////////////////////////////////////////////////////////////////////////////////////////////
+
+            /*// Create the ToolTip and associate with the Form container.
+            ToolTip toolTip2 = new ToolTip();
+
+            // Set up the delays for the ToolTip.
+            toolTip2.AutoPopDelay = 5000;
+            toolTip2.InitialDelay = 100;
+            toolTip2.ReshowDelay = 50;
+            toolTip2.BackColor = Color.Azure;
+            // Force the ToolTip text to be displayed whether or not the form is active.
+            toolTip2.ShowAlways = true;
+
+            // Set up the ToolTip text for the Button and Checkbox.
+            toolTip2.SetToolTip(this.button1, " ");
+            toolTip2.ToolTipTitle = "4444444";*/
+
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //lab12_1
+            /*Point p = pictureBox1.Location;   // позиция левого верхнего угла pictureBox1
+            Point p1 = this.Location;        // позиция левого верхнего угла формы
+            // e.X и e.Y позиция курсора в pictureBox1
+            int XX, YY;
+            XX = e.X + p.X + p1.X; YY = e.Y + p.Y + p1.Y;
+            toolTip1.ToolTipTitle = Convert.ToString(XX) + "; " + Convert.ToString(YY);
+            toolTip1.SetToolTip(this.pictureBox1, " ");*/
+
+            //lab12_2
+            Point p = pictureBox1.Location;   // позиция левого верхнего угла pictureBox1
+            Point p1 = this.Location;        // позиция левого верхнего угла формы
+            // e.X и e.Y позиция курсора в pictureBox1
+            int XX, YY;
+            XX = e.X + p.X + p1.X; YY = e.Y + p.Y + p1.Y;
+            /*Bitmap bmp = new Bitmap(1, 1);
+            using (Graphics g = Graphics.FromImage(bmp))
+            { g.CopyFromScreen(XX, YY, 0, 0, new Size(1, 1)); }
+            Color color = bmp.GetPixel(0, 0);
+            string SS;
+            SS = Convert.ToString(color);
+            toolTip1.ToolTipTitle = SS;*/
+
+            toolTip1.ToolTipTitle = "x=" + Convert.ToString(XX) + "; " + "y=" + Convert.ToString(YY);
+            toolTip1.SetToolTip(this.pictureBox1, " ");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Окрываем диалоговое окно
+            OpenFileDialog ofn = new OpenFileDialog();
+            ofn.InitialDirectory = "D:\\";              //Диск D:
+            ofn.Filter = "txt files |*.txt";          //Указываем тип открываемых файлов
+            ofn.FilterIndex = 2;
+
+            if (ofn.ShowDialog() == DialogResult.OK)
+            {
+                textBox3.Text = ofn.FileName;
+                richTextBox1.Text = File.ReadAllText(ofn.FileName);
+                str = richTextBox1.Text;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            StreamReader data = new StreamReader(str, Encoding.GetEncoding(1251));
+            string str1 = data.ReadLine();
+            int i;
+
+            string[] mas = str1.Split(' ');
+            for (i = 0; i < mas.Length; i++)
+            {
+                str1 +=mas[i].Replace('m', 'M') + " ";
+            }
+            str1 = richTextBox1.Text;
+
+
+            //Алгоритм фильтрации
+            string pattern = @"<[^>]*>";
+            Regex newReg = new Regex(pattern);
+            richTextBox1.Text = Regex.Replace(str, @"<[^>]*>", String.Empty);
+        }
+    }
+}
---
+++ C#/L000110_lab_13/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace L000110_lab_13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(15, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "X =";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(396, 144);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Computing group";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(49, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(325, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(289, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 25);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(15, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Y =";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(49, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 25);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(49, 73);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(325, 23);
+            this.textBox2.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.SpringGreen;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 155);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(419, 215);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(168, 205);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(35, 35);
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseDown);
+            this.pictureBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseMove);
+            this.pictureBox2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 370);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "L000110";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
