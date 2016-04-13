cc428b7de608f2ce2388638066b90897d388e7f6
olegp <olegp@MSSO>
2016-04-13 10:13:14 +0300

    lab1

---
+++ JAVA/L00001_lab01/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+	public static void main(String[] args){
+		System.out.println("****************************************************************************");
+		System.out.println("Bodily exercise, when compulsory, does no harm to the body; but knowledge,\nwhich is acquired under compulsion, obtains no hold on the mind.");
+		System.out.println("...Plato...");
+		System.out.println("****************************************************************************");
+	}
+}
