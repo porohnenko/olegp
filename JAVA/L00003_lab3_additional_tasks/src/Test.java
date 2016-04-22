9581730e03ea183b7d6c92e9b219ef16a5bdbb30
olegp <olegp@MSSO>
2016-04-22 12:04:15 +0300

    task1

---
+++ JAVA/L00003_lab3_additional_tasks/src/Test.java
@@ -0,0 +1,19 @@
+
+/**
+ * Lab work №3. Additional task 1
+ * Theme: Output arguments of command line in revers
+ * Group: PV1-15PO
+ * Author: Oleg Porohnenko
+ * Date: Apr 22, 2016
+ * Version: 1.0
+ */
+class Test {
+
+    public static void main(String[] args) {
+
+        for (int i = args.length - 1; i >= 0; i--) {
+            //int j = args.length - i;
+            System.out.printf("Argument #%d: %s\n", i+1, args[i]);
+        }
+    }
+}
