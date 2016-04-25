5126b496e20aa7a7f32839e4745d37190b66fc74
porohnenko <porohnenko@mail.ru>
2016-04-25 13:46:19 +0300

    lab6

---
+++ JAVA/L00006_lab6/src/by/bsu/ibmt/groupPV115PO/porohnenko/game/Anagrams.java
@@ -0,0 +1,15 @@
+/**
+ * Lab work №6
+ * Theme: 
+ * Group: PV1-15PO
+ * Author: Oleg Porohnenko
+ * Date: Apr 24, 2016
+ * Version: 1.0
+ */
+
+package by.bsu.ibmt.groupPV115PO.porohnenko.game;
+
+
+public class Anagrams {
+
+}
