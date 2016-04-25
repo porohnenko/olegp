6a8ba5d57ed1abbd698511bd9565af9af7a9509f
porohnenko <porohnenko@mail.ru>
2016-04-26 00:15:58 +0300

    lab6_ind

--- Database/Practice #6_ind.sql
+++ Database/Practice #6_ind.sql
@@ -58,4 +58,61 @@ select distinct [T].TeachName [Имя преподавателя]
 		on U.PIN = T.PIN
 where U.NTerm between '1' and '2'
 ----------------------------------------------------------------------------------
--------------------------------------
+
+
+
+
+
+
+
+
+
+----------------------------------------------------------------------------------
+--Задание 3
+--Вывести ФИО, личные номера и должность всех преподавателей, у которых оклад выше 11 млн.руб.
+
+select T.TeachName [ФИО], T.Post [Должность], T.EmployeeNumber [Личный номер], S.SalaryRate [Оклад]
+	from Teachers T inner join SalaryCosts S
+		on T.TeachName=S.TeachName
+	where SalaryRate >= 11000000
+	
+----------------------------------------------------------------------------------
+--Задание 4
+--Вывести имена всех преподавателей, которые ведут дисциплины на факультете 'ФКСИС'.
+
+select T.TeachName [ФИО], S.Department [Факультет]
+	from Teachers T join SalaryCosts S 
+		on T.TeachName = S.TeachName
+		where S.Department = 'ФКСИС'
+group by T.TeachName,S.Department
+----------------------------------------------------------------------------------
+--Задание 5
+--Вывести ФИО и личные номера преподавателей факультета ФКСИС, у которых зарплата выше 12 млн.руб. Личные номера расположить в порядке возрастания.
+
+select distinct T.EmployeeNumber [Личный номер], S.TeachName [ФИО], T.Salary [з/п, руб.]
+	from SalaryCosts S inner join Teachers T
+		on S.TeachName = T.TeachName 
+		where S.Department = 'ФКСИС'
+order by T.EmployeeNumber
+----------------------------------------------------------------------------------
+--Задание 6
+--Вывести имена руководителей факультетов и название факультетов, ФИО преподавателей которые работали в текущем месяце сверх нормы.
+
+select distinct T.TeachName [ФИО преподавателя], D.NameDepartment [Факультет], D.Head [Руководитель], S.FullTimer [Отработанное время, ч]
+	from SalaryCosts S join Teachers T
+		on S.TeachName = T.TeachName
+		join Departments D
+		on S.Department = D.NameDepartment
+where FullTimer > 168
+order by D.NameDepartment
+----------------------------------------------------------------------------------
+--Задание 7
+--Вывести имена преподавателей которые преподают как на ИЭФ так и на ФТ.
+
+select distinct T.TeachName [Имя преподавателя]
+	from SalaryCosts S join Teachers T
+	on S.TeachName = T.TeachName
+where S.Department = 'ИЭФ' and 
+T.TeachName = (select S2.TeachName from SalaryCosts S2 where S2.TeachName = T.TeachName and S2.Department= 'ФТ' )
+
+----------------------------------------------------------------------------------
