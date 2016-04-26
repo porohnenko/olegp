549b8f6b670b6cd571150332145cd39b198cd7d4
olegp <olegp@MSSO>
2016-04-26 16:18:35 +0300

    lab6

---
+++ JAVA/L00006_lab6/src/by/bsu/ibmt/groupPV115PO/porohnenko/view/AnagramsConsolePrint.java
@@ -0,0 +1,15 @@
+/**
+ * Lab work № ...
+ * Theme: ...
+ * Group: PV1-15PO
+ * Author: Oleg Porohnenko
+ * Date: Apr 26, 2016
+ * Version: 1.0
+ */
+
+package by.bsu.ibmt.groupPV115PO.porohnenko.view;
+
+
+public class AnagramsConsolePrint {
+
+}
