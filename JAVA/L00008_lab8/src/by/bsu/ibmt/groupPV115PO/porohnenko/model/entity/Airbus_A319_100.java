83352d9ca31d393b0586c0c153714fb0851613e9
porohnenko <porohnenko@mail.ru>
2016-05-15 19:07:04 +0300

    lab8

---
+++ JAVA/L00008_lab8/src/by/bsu/ibmt/groupPV115PO/porohnenko/model/entity/Airbus_A319_100.java
@@ -0,0 +1,22 @@
+/**
+ * Lab work №8
+ * Theme: Airline (Austrian Airlines)
+ * Group: PV1-15PO
+ * Author: Oleg Porohnenko
+ * Date: May 15, 2016
+ * Version: 1.0
+ */
+
+package by.bsu.ibmt.groupPV115PO.porohnenko.model.entity;
+
+
+public class Airbus_A319_100 {
+    
+    private int passengerCapacity;
+    private int loadingCapacity;
+    private int flyingRange;
+    
+    
+    
+
+}
---
+++ JAVA/L00008_lab8/src/by/bsu/ibmt/groupPV115PO/porohnenko/model/entity/Boeing_767_300.java
@@ -0,0 +1,15 @@
+/**
+ * Lab work №
+ * Theme: 
+ * Group: PV1-15PO
+ * Author: Oleg Porohnenko
+ * Date: May 15, 2016
+ * Version: 1.0
+ */
+
+package by.bsu.ibmt.groupPV115PO.porohnenko.model.entity;
+
+
+public class Boeing_767_300 {
+
+}
