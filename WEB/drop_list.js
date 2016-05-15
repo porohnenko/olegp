7b360c241ff72cfd46b1540a169c8956dc4d49ee
porohnenko <porohnenko@mail.ru>
2016-05-15 19:07:36 +0300

    15.05.2016

---
+++ WEB/drop_list.js
@@ -0,0 +1,16 @@
+<head>
+<script type="text/javascript">
+function Menu(id)
+{
+var menu = document.getElementById('menu_' + id).style;
+if (menu.display == 'none')
+{
+menu.display = 'block';
+}
+else
+{
+menu.display = 'none';
+}
+}
+</script>
+</head>
