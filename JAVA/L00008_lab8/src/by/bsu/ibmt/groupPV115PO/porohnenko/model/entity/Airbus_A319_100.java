9f165c537706a08a7e0e3c45079771152b06905a
porohnenko <porohnenko@mail.ru>
2016-05-15 19:28:12 +0300

    lab8

--- JAVA/L00008_lab8/src/by/bsu/ibmt/groupPV115PO/porohnenko/model/entity/Airbus_A319_100.java
+++ JAVA/L00008_lab8/src/by/bsu/ibmt/groupPV115PO/porohnenko/model/entity/Airbus_A319_100.java
@@ -6,17 +6,8 @@
  * Date: May 15, 2016
  * Version: 1.0
  */
-
 package by.bsu.ibmt.groupPV115PO.porohnenko.model.entity;
 
-
 public class Airbus_A319_100 {
-    
-    private int passengerCapacity;
-    private int loadingCapacity;
-    private int flyingRange;
-    
-    
-    
 
 }
---
+++ JAVA/L00008_lab8/src/by/bsu/ibmt/groupPV115PO/porohnenko/model/entity/abs/Aircraft.java
@@ -0,0 +1,16 @@
+/**
+ * Lab work №
+ * Theme:
+ * Group: PV1-15PO
+ * Author: Oleg Porohnenko
+ * Date: May 15, 2016
+ * Version: 1.0
+ */
+package by.bsu.ibmt.groupPV115PO.porohnenko.model.entity.abs;
+
+public class Aircraft {
+
+    private int passengerCapacity;
+    private int loadingCapacity;
+    private int flyingRange;
+}
